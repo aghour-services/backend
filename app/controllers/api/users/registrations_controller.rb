ad55ee46df7d13723c9b86b0cddf4c2c91463bc3
Ahmed Magdy <ahmadmgdi94@gmail.com>
2021-12-19 18:17:07 +0200

    User Sign in

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -32,7 +32,7 @@ class Api::Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[mobile name password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email mobile name password])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -1,27 +1,12 @@
 # frozen_string_literal: true
 
 class Api::Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def create
+    warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
+    current_user.generate_token
+    current_user.save
+    current_user.reload
+    render json: { current_user: current_user }, status: 200
+  end
 end

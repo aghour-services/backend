c4e98997c21b6c4ad6f94e596ecc525b9fa0d469
Ahmed Magdy <ahmadmgdi94@gmail.com>
2021-12-19 18:19:43 +0200

    Validates name presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
   before_create :generate_token
 
   def generate_token
-    self.token = id.to_s + '/' + SecureRandom.urlsafe_base64(nil, false)
+    self.token = "#{id}/#{SecureRandom.urlsafe_base64(nil, false)}"
   end
 end

21c47ecb830137996186c62cc3d3303b91c77238
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-01-27 01:47:08 +0200

    render app-ads.txt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < HtmlController
   before_action :fetch_user, only: %I[destroy]
 
   def main
-    
+    file_path = "#{Rails.root}/public/app-ads.txt"
+
+    render file: file_path, layout: false, content_type: 'text/plain'
   end
 end

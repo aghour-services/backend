ea978dc5d72fa6a32f14099d4124eb498125a4e9
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-01-27 02:27:59 +0200

    App ads

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < HtmlController
   before_action :fetch_user, only: %I[destroy]
 
   def main
-    file_path = "#{Rails.root}/public/files/app-ads.txt"
+  #   file_path = "#{Rails.root}/public/files/app-ads.txt"
 
-    send_file file_path
+  #   send_data file_path, layout: false, type: 'plain/text'
   end
 end

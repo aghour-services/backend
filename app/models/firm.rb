c4f6eb69714b38dda6fd7e7809d942179a52f028
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-07-15 19:32:40 +0200

    Handled exception

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -7,7 +7,7 @@ class Firm < ApplicationRecord
   belongs_to :user, optional: true
   enum status: { draft: 0, published: 1 }, _default: :draft
 
-  scope :filter_by, ->(tags) { search_by_tags(normalize_tags(tags)) unless tags.nil? }
+  scope :filter_by, ->(tags) { search_by_tags(tags.split(',')) unless tags.nil? }
 
   include PgSearch::Model
   pg_search_scope :search, against: %i[name description],
@@ -15,10 +15,4 @@ class Firm < ApplicationRecord
 
   pg_search_scope :search_by_tags, against: %i[tags],
                                    using: { tsearch: { any_word: true, prefix: true } }
-
-  private
-
-  def normalize_tags(tags)
-    tags.split(',')
-  end
 end

5d7b1ee87e0c3a197d85bd544798da8bc2ff9cd0
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-11-29 18:55:26 +0100

    Enable public file serving

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class PagesController < HtmlController
-  before_action :fetch_user, only: %I[destroy]
-
   def main
-    file_path = "#{Rails.root}/public/files/app-ads.txt"
+    # render json: { data: params }
+    # file_path = "#{Rails.root}/public/files/app-ads.txt"
 
-    send_data file_path, layout: false, type: 'plain/text'
+    # send_data file_path, layout: false, type: 'plain/text'
   end
 end

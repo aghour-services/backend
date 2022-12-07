cb62ecd2931248f23f219355e6e9f1182e39d50b
m7madmagdy <mohamedmagdysaber@gmail.com>
2022-12-07 21:54:32 +0200

    Changed find_or_create to create

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,11 +8,10 @@ class ApiController < ActionController::API
   def authenticate_user!
     self.current_user = User.find_by token: request.headers['TOKEN']
     unless current_user
-          AuthenticationLog.find_or_create_by(user_token: request.headers['TOKEN'])
-    head :unauthorized 
+        AuthenticationLog.create(user_token: request.headers['TOKEN'])
+        head :unauthorized 
+    end
   end
-
-end
 
   def user_ability
     @user_ability = UserAbility.new(current_user)

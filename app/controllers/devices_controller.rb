96074b3de1384b2815226671b4a4d6dbba92f7b1
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2022-12-15 01:05:11 +0200

    sorted devices table

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,5 +1,5 @@
 class DevicesController < HtmlController
     def index
-      @devices = Device.order(last_usage_time: :desc)
+      @devices = Device.all.order(last_usage_time: :desc).limit(200)
     end
 end

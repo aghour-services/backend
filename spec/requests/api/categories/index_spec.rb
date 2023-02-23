e6c264427525604583bcf98a5bfe49f5d4da2844
Ahmed Magdy <ahmadmgdi94@gmail.com>
2023-02-23 18:45:21 +0100

    Enabled categories api index spec

--- spec/requests/api/categories/index_spec.rb
+++ spec/requests/api/categories/index_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe 'CategoriesController', type: :request do
-  # describe "GET index" do
-  #   let(:categories) { create_list(:category, 5) } # create 5 categories for testing
+  describe 'GET index' do
+    let(:categories) { create_list(:category, 2) }
 
-  #   it "returns a successful response" do
-  #     get '/api/categories'
-  #     expect(response).to have_http_status(:ok)
-  #   end
-  # end
+    it 'returns a successful response' do
+      get '/api/categories'
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end

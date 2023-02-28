87ec8d9a19e1ac24d23f109ec76b27173cc7adfa
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-02-28 23:09:35 +0200

    Covered sessions controller specs

--- spec/requests/api/users/sessions_spec.rb
+++ spec/requests/api/users/sessions_spec.rb
@@ -1,2 +1,24 @@
-RSpec.describe 'Api::Users', type: :request do
+RSpec.describe "Api::Users", type: :request do
+  let(:user) { create(:user) }
+
+  describe "#sign_in" do
+    context "#success" do
+      before { post "/api/users/sign_in", params: { user: { email: user.email, password: user.password } } }
+
+      it "generates a new authentication token for the user" do
+        expect(user.reload.token).not_to be_nil
+      end
+
+      it "returns 200 status code" do
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "#failure" do
+      before { post "/api/users/sign_in", params: { user: { email: user.email, password: "invalid" } } }
+      it "returns 401 unauthorized status code" do
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end

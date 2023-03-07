20918ad7631e70218292543a359d8d6059b44099
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2023-03-07 15:31:00 +0200

    Test Likes Controller

---
+++ spec/requests/api/likes/destroy_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe 'Api::Likes', type: :request do
+    let(:user) { create(:user) }
+    let(:article) { create(:article, user:) }
+    let!(:like) {create(:like, article:, user:)}
+    let(:headers) {{TOKEN: user.token}}
+
+    describe '#destroy' do
+      context '#success' do
+        context 'with authorized user' do
+          it 'deletes the like' do
+            delete "/api/articles/#{article.id}/unlike", headers: headers
+            expect(response).to have_http_status(:ok)
+            expect(Like.count).to eq(0)
+          end
+        end
+      end
+
+
+      context "#failure" do
+        context "with unauthorized user" do
+          it "don't deletes the like" do
+            delete "/api/articles/#{article.id}/unlike"
+            expect(response).to have_http_status(:unauthorized)
+            expect(Like.count).to eq(1)
+          end
+        end
+      end
+    end
+  end

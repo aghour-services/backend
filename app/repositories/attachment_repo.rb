a5dc9d27a5c72b0ef166d7d3d01d9d41a6f2decb
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-03-27 17:41:40 +0200

    Created Attachment repo

---
+++ app/repositories/attachment_repo.rb
@@ -0,0 +1,17 @@
+class AttachmentRepo
+    def initialize(article, raw_response, resource_id, resource_type)
+        @article = article
+        @raw_response = raw_response
+        @resource_id = resource_id
+        @resource_type = resource_type
+    end
+
+    def create_attachment
+        attachment = @article.attachments.create(
+            raw_response: @raw_response, 
+            resource_id: @resource_id, 
+            resource_type: @resource_type)
+        resource_url = attachment.resource_url   
+    end
+end
+

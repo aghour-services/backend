48fc82451d0c67566592200445e49e0715ada766
Ahmed Magdy <ahmadmgdi94@gmail.com>
2023-04-26 03:49:10 +0200

    Make specs green again (#107)

--- app/services/imgur_uploader.rb
+++ app/services/imgur_uploader.rb
@@ -1,21 +1,21 @@
-require "net/http"
-require "uri"
-require "base64"
-require "json"
+require 'net/http'
+require 'uri'
+require 'base64'
+require 'json'
 
 class ImgurUploader
-  BASE_URL = "https://api.imgur.com/3/upload.json"
+  BASE_URL = 'https://api.imgur.com/3/upload.json'
 
   def self.upload(image_path)
-    imagedata = Base64.encode64(File.read(image_path, mode: "rb"))
+    imagedata = Base64.encode64(File.read(image_path, mode: 'rb'))
     params = { image: imagedata }
     uri = URI.parse(BASE_URL)
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
 
     request = Net::HTTP::Post.new(uri.path)
-    request["Authorization"] = ENV["IMGUR_CLIENT_ID"]
-    request["Authorization"] = ENV["IMGUR_ACCESS_TOKEN"]
+    # request['Authorization'] = ENV['IMGUR_CLIENT_ID']
+    request['Authorization'] = ENV['IMGUR_ACCESS_TOKEN']
     request.set_form_data(params)
     response = https.request(request)
     JSON.parse(response.body)

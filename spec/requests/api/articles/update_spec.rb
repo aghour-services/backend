c0a2b47de58db1ac06c31e3136388f807860a97c
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-08-06 21:45:29 +0300

    Fixed N+1 query in articles & Improve database performance (#133)

--- spec/requests/api/articles/update_spec.rb
+++ spec/requests/api/articles/update_spec.rb
@@ -1,10 +1,20 @@
 RSpec.describe 'Api::Articles', type: :request do
   let(:user) { create(:user) }
   let(:headers) { { TOKEN: user.token } }
-
   let(:article) { create(:article, user:, description: 'old desc') }
+  let(:attachment) { fixture_file_upload('spec/fixture_files/1.jpeg', 'image/png') }
+
+  let(:imgur_response) do
+    '{"data":{"id":"3O9qUDG", "type":"image/png"}, "success": true}'
+  end
 
   describe '#update' do
+    before do
+      stub_request(:post, 'https://api.imgur.com/3/upload.json')
+        .to_return(body: imgur_response,
+                   headers: { "Content-Type": 'application/json' })
+    end
+
     context '#success' do
       context 'with authorized usr' do
         context 'with valid parameters' do
@@ -12,9 +22,20 @@ RSpec.describe 'Api::Articles', type: :request do
             expect do
               article.description = 'new desc'
               article_params = article.attributes.symbolize_keys
-              put "/api/articles/#{article.id}", params: { article: article_params }, headers: headers
+              put("/api/articles/#{article.id}", params: { article: article_params.merge(attachment:) },
+                                                 headers:)
               expect(response).to have_http_status(:ok)
             end.to change { article.description }.to('new desc')
+          end
+
+          it 'updates the article with attachment' do
+            expect do
+              article.description = 'new desc'
+              article_params = article.attributes.symbolize_keys
+              put("/api/articles/#{article.id}", params: { article: article_params.merge(attachment:) },
+                                                 headers:)
+              expect(response).to have_http_status(:ok)
+            end.to change { Attachment.count }.by(1)
           end
         end
       end
@@ -27,9 +48,20 @@ RSpec.describe 'Api::Articles', type: :request do
             expect do
               article.description = 'new desc'
               article_params = article.attributes.symbolize_keys
-              put "/api/articles/#{article.id}", params: { article: article_params }, headers: headers
+              put("/api/articles/#{article.id}", params: { article: article_params.merge(attachment:) },
+                                                 headers:)
               expect(response).to have_http_status(:ok)
             end.to change { article.description }.to('new desc')
+          end
+
+          it 'updates the article with attachment' do
+            expect do
+              article.description = 'new desc'
+              article_params = article.attributes.symbolize_keys
+              put("/api/articles/#{article.id}", params: { article: article_params.merge(attachment:) },
+                                                 headers:)
+              expect(response).to have_http_status(:ok)
+            end.to change { Attachment.count }.by(1)
           end
         end
       end
@@ -49,7 +81,7 @@ RSpec.describe 'Api::Articles', type: :request do
         it 'doesnt create a categories' do
           article.description = ''
           article_params = article.attributes.symbolize_keys
-          put "/api/articles/#{article.id}", params: { article: article_params }, headers: headers
+          put("/api/articles/#{article.id}", params: { article: article_params }, headers:)
 
           expect(response).to have_http_status(:unprocessable_entity)
         end
@@ -61,9 +93,25 @@ RSpec.describe 'Api::Articles', type: :request do
 
         it 'doesnt create a categories' do
           article_params = article.attributes.symbolize_keys
-          put "/api/articles/#{article.id}", params: { article: article_params }, headers: headers
+          put("/api/articles/#{article.id}", params: { article: article_params }, headers:)
 
           expect(response).to have_http_status(:unauthorized)
+        end
+      end
+
+      context 'when imgur response is false' do
+        let(:imgur_response) do
+          '{"data":{"id":"3O9qUDG", "type":"image/png"}, "success": false}'
+        end
+
+        it 'doesnt update an article and Rollback' do
+          article.description = 'new description'
+          article_params = article.attributes.symbolize_keys
+          put("/api/articles/#{article.id}", params: { article: article_params.merge(attachment:) },
+                                             headers:)
+
+          expect(Article.last.description).to eq('old desc')
+          expect(response).to have_http_status(:unprocessable_entity)
         end
       end
     end

9a0a8241eb1eea00bfcb80e8c19dd4bba390bc1b
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-11-10 22:22:33 +0200

    Covered notification service

--- spec/services/article_notification_spec.rb
+++ spec/services/article_notification_spec.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe ArticleNotification do
+  describe '#send' do
+    let(:data) { { title: 'Test Article', body: 'This is a test article.' } }
+    let(:notification) { instance_double('Hash') }
+    let(:fcm) { instance_double('FCM') }
+
+    before do
+      allow(Rails).to receive_message_chain(:env, :production?).and_return(true)
+      allow(Rails.application.credentials).to receive(:fcm_server_key).and_return('test_key')
+      allow_any_instance_of(ArticleNotification).to receive(:construct_notification_from_params).with(data).and_return(notification)
+      allow(FCM).to receive(:new).with('test_key').and_return(fcm)
+    end
+
+    it 'sends a notification to the News-v2 topic' do
+      expect(fcm).to receive(:send_to_topic).with('News-v2', notification)
+      ArticleNotification.new(data).send
+    end
+
+    context 'when not in production environment' do
+      before do
+        allow(Rails).to receive_message_chain(:env, :production?).and_return(false)
+      end
+
+      it 'does not send a notification' do
+        expect(fcm).not_to receive(:send_to_topic)
+        ArticleNotification.new(data).send
+      end
+    end
+  end
 end

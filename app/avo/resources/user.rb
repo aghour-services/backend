0ca0100069ae29b444367edace9b12251eeba1da
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-12-21 19:17:52 +0200

    Fixed resources on dashboard

--- app/avo/resources/user.rb
+++ app/avo/resources/user.rb
@@ -5,20 +5,31 @@ class Avo::Resources::User < Avo::BaseResource
   # }
 
   def fields
-    field :id, as: :id
-    field :avatar, as: :external_image, link_to_resource: true, width: 60, height: 60, radius: 60 do
-      record&.avatar&.url
+    main_panel do
+      field :id, as: :id
+      field :avatar, as: :external_image, link_to_resource: true, as_avatar: :circle, width: 60, height: 60,
+                     radius: 60, only_on: :index do
+        record&.avatar&.url
+      end
+      field :name, as: :text
+      field :mobile, as: :text
+      field :email, as: :text
+      field :token, as: :text, hide_on: :all
+      field :role, as: :select, enum: ::User.roles, hide_on: %i[index show]
+
+      sidebar do
+        field :avatar, as: :external_image, link_to_resource: true, as_avatar: :circle,
+                       width: 60, height: 60, radius: 60 do
+          record&.avatar&.url
+        end
+        field :admin, as: :boolean, only_on: %i[index show] do
+          record&.role == 'admin'
+        end
+      end
     end
-    field :name, as: :text
-    field :admin, as: :boolean, only_on: %i[index show] do
-      record&.role == 'admin'
-    end
-    field :mobile, as: :text
-    field :email, as: :text
-    field :token, as: :text
-    field :role, as: :select, enum: ::User.roles, hide_on: %i[index show]
-    field :firms, as: :has_many
+
     field :articles, as: :has_many
+    field :firms, as: :has_many
     field :comments, as: :has_many
     field :likes, as: :has_many
     field :devices, as: :has_many

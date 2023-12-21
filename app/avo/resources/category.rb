e3cadd3db75e0f51e4755f9bfdc0ac1185f96f06
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-12-21 19:34:51 +0200

    Fixed cagtegory resource

--- app/avo/resources/category.rb
+++ app/avo/resources/category.rb
@@ -7,7 +7,11 @@ class Avo::Resources::Category < Avo::BaseResource
   def fields
     field :id, as: :id
     field :name, as: :text
-    field :icon, as: :external_image
+    field :icon, as: :file, accept: 'image/*', hide_on: :index
+    field :icon_path, as: :external_image, link_to_resource: true,
+                      width: 110, height: 110, radius: 8, only_on: :index do
+      record&.icon_path
+    end
     field :firms, as: :has_many
   end
 end
